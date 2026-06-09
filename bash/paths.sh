248afa2064cadee3cb0a81bc57481a246f645a3a
Kiril Molchanov <molchkir@fit.cvut.cz>
2026-06-09 20:15:53 +0200

    [general]: Tweaks to make things work

--- bash/paths.sh
+++ bash/paths.sh
@@ -1,11 +1,17 @@
-
 # append destinations to $PATH
-export MY_PATH="$HOME/.local/bin"
 
-for x in "$MY_PATH"/* ; do
+export MY_LOCAL_PATH="$HOME/.local/bin"
+export MY_DOTFILES_PATH="$MY_DOTFILES/bin"
+
+for x in "$MY_DOTFILES_PATH"/* ; do
 	if [[ -d "$x" ]] ; then
-		MY_PATH="$MY_PATH:$x"
+		MY_DOTFILES_PATH="$MY_DOTFILES_PATH:$x"
 	fi
 done
-export PATH="$PATH:$MY_PATH"
+for x in "$MY_LOCAL_PATH"/* ; do
+	if [[ -d "$x" ]] ; then
+		MY_LOCAL_PATH="$MY_LOCAL_PATH:$x"
+	fi
+done
+export PATH="$PATH:$MY_DOTFILES_PATH:$MY_LOCAL_PATH"
 
